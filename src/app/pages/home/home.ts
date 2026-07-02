03e0ee3ae16ed04db3bebe32c0d6a32b4875c747
Sai Manoj <saimanoj.burre@gmail.com>
2026-07-02 15:34:28 +0530

    first commit

---
+++ src/app/pages/home/home.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Navbar } from '../../core/navbar/navbar';
+
+@Component({
+  selector: 'app-home',
+  imports: [Navbar],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {}
